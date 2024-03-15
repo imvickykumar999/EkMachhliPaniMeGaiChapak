c294d611acf5a9e50a5a59c93acd03fcd1ecc2a3
Vicky Kumar <imvickykumar999@gmail.com>
2024-03-15 15:45:53 +0530

    Update infinite_series.py

--- infinite_series.py
+++ infinite_series.py
@@ -1,14 +1,25 @@
+# main.py
 
-n = 0
-while True:
-    n += 1
-    
-    print(n, 'Machhli')
+def main():
+    n = 0
+    t = 6
+    c = 0
 
-    for i in range(1, n+1):
-        print('Pani me gai')
-
-    for i in range(1, n+1):
-        print('Chhapak')
+    while True:
+        n += 1
         
-    input()
+        print('Player', c%t +1, ':', n, 'Machhli')
+        c += 1
+
+        for i in range(1, n+1):
+            print('Player', c%t +1, ':', 'Pani me gai')
+            c += 1
+
+        for i in range(1, n+1):
+            print('Player', c%t +1, ':', 'Chhapak')
+            c += 1
+            
+        input()
+
+if __name__ == "__main__":
+    main()
