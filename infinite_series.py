62d26aa55157bbfc91fada8c324e2faa38ac6bea
Vicky Kumar <imvickykumar999@gmail.com>
2024-03-15 13:15:10 +0530

    Create infinite_series.py

---
+++ infinite_series.py
@@ -0,0 +1,14 @@
+
+n = 0
+while True:
+    n += 1
+    
+    print(n, 'Machhli')
+
+    for i in range(1, n+1):
+        print('Pani me gai')
+
+    for i in range(1, n+1):
+        print('Chhapak')
+        
+    input()
